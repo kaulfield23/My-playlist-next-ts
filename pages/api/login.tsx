695ae368daefa137f6aec056bbc6c0d6fd0a219e
kaulfield23 <kaulfield23@gmail.com>
2022-05-18 14:37:21 +0200

    little changes

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { env } from "process";
 import SpotifyWebApi from "spotify-web-api-node";
 
 export default async function loginHandler(
