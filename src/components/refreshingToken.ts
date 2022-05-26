a5ae0cf175c2244e759568fea9dac4f8653d0ece
kaulfield23 <kaulfield23@gmail.com>
2022-05-26 16:44:06 +0200

    changed filename

---
+++ src/components/refreshingToken.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+export async function RefreshingToken(refreshToken: string, expiresIn: number) {
+  useEffect(() => {
+    const fetchData = () => {
+      const interval = setInterval(async () => {
+        const res = await fetch(`/api/refresh`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            refreshToken,
+          }),
+        });
+      }, (expiresIn - 60) * 1000);
+      return () => clearInterval(interval);
+    };
+    fetchData();
+  }, [refreshToken, expiresIn]);
+}
