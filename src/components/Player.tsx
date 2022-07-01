ac62d443582cc27c7d14c54f9a1815a68a460171
kaulfield23 <kaulfield23@gmail.com>
2022-07-01 16:45:13 +0200

    added volume slider and change volume

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,6 +1,6 @@
 import { Grid, Input, Slider, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import SpotifyPlayer from "react-spotify-web-playback";
 import VolumeUp from "@mui/icons-material/VolumeUp";
 
@@ -11,83 +11,78 @@ type PlayerType = {
 
 const Player: FC<PlayerType> = ({ accessToken, trackUri }) => {
   const [play, setPlay] = useState(false);
-  const [value, setValue] = useState<number | string | Array<number | string>>(
-    30
-  );
+  const [value, setValue] = useState<number>(30);
 
   const handleSliderChange = (event: Event, newValue: number | number[]) => {
-    setValue(newValue);
+    setValue(newValue as number);
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value === "" ? "" : Number(event.target.value));
-  };
-
-  const handleBlur = () => {
-    if (value < 0) {
-      setValue(0);
-    } else if (value > 100) {
-      setValue(100);
-    }
-  };
   useEffect(() => {
     setPlay(true);
   }, [trackUri]);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const changeVolume = async () => {
+        try {
+          await fetch(
+            `https://api.spotify.com/v1/me/player/volume?volume_percent=${value}`,
+            {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      changeVolume();
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [value, accessToken]);
+
   if (!accessToken) {
     return null;
   }
-
   return (
     <>
       <Box className="player">
         <SpotifyPlayer
           token={accessToken}
           showSaveIcon
+          initialVolume={0.7}
           styles={{
             sliderColor: "purple",
             activeColor: "green",
             trackArtistColor: "grey",
             height: "50px",
+            bgColor: "#e0eae8",
+            color: "purple",
           }}
+          magnifySliderOnHover={true}
           callback={(state) => {
             if (!state.isPlaying) setPlay(false);
           }}
           play={play}
           uris={trackUri ? [trackUri] : []}
         />
-      </Box>
-      <Box sx={{ width: 250 }}>
-        <Typography id="input-slider" gutterBottom>
-          Volume
-        </Typography>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item>
-            <VolumeUp />
+        <Box sx={{ width: 200 }} className="volume">
+          <Grid container alignItems="center">
+            <Grid item>
+              <VolumeUp />
+            </Grid>
+            <Grid item xs>
+              <Slider
+                value={typeof value === "number" ? value : 0}
+                onChange={handleSliderChange}
+                aria-labelledby="input-slider"
+              />
+            </Grid>
           </Grid>
-          <Grid item xs>
-            <Slider
-              value={typeof value === "number" ? value : 0}
-              onChange={handleSliderChange}
-              aria-labelledby="input-slider"
-            />
-          </Grid>
-          <Grid item>
-            <Input
-              value={value}
-              size="small"
-              onChange={handleInputChange}
-              onBlur={handleBlur}
-              inputProps={{
-                step: 10,
-                min: 0,
-                max: 100,
-                type: "number",
-                "aria-labelledby": "input-slider",
-              }}
-            />
-          </Grid>
-        </Grid>
+        </Box>
       </Box>
     </>
   );
